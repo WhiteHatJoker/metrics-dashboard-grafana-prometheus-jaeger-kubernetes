bcc9f176fa02ccc06d16af09e552b30b56e2c2f5
WhiteHatJoker <rkhudoyberdiev@gmail.com>
2021-10-10 01:58:52 +0500

    added flask exporter and jaeger tracing to backend app

--- reference-app/backend/app.py
+++ reference-app/backend/app.py
@@ -1,14 +1,37 @@
 from flask import Flask, render_template, request, jsonify
+import logging
+
+from prometheus_flask_exporter import PrometheusMetrics
+
+from jaeger_client import Config
+from flask_opentracing import FlaskTracing
 
 import pymongo
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
-
 app.config['MONGO_DBNAME'] = 'example-mongodb'
 app.config['MONGO_URI'] = 'mongodb://example-mongodb-svc.default.svc.cluster.local:27017/example-mongodb'
-
 mongo = PyMongo(app)
+
+logging.basicConfig(level=logging.INFO)
+metrics = PrometheusMetrics(app)
+metrics.info("app_info", "Backend Service", version="1.0.0")
+
+config = Config(
+    config={
+            'sampler': {
+                'type': 'const',
+                'param': 1,
+            },
+            'logging': True,
+            'reporter_batch_size': 1,
+        },
+    service_name="service"
+)
+
+jaeger_tracer = config.initialize_tracer()
+tracing = FlaskTracing(jaeger_tracer, True, app)
 
 @app.route('/')
 def homepage():
