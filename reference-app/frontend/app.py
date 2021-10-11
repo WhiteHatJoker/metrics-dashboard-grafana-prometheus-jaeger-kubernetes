9a4974fc4f9e538f98974f62edeecbed7c85ab2f
WhiteHatJoker <rkhudoyberdiev@gmail.com>
2021-10-11 19:51:25 +0500

    Try just prometheusmetrics otherwise no flask_http in prometheus

--- reference-app/frontend/app.py
+++ reference-app/frontend/app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, request
-from prometheus_flask_exporter.multiprocess import GunicornInternalPrometheusMetrics
+from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
-metrics = GunicornInternalPrometheusMetrics(app)
+metrics = PrometheusMetrics(app)
 metrics.info('app_info', 'Frontend Service', version='1.0.3')
 
 @app.route('/')
