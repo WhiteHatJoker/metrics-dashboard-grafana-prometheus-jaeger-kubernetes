23a32626c0f089b82e73b18f6d1597b9e2a18ae8
WhiteHatJoker <rkhudoyberdiev@gmail.com>
2021-10-11 17:05:41 +0500

    conditional logic based on gunicorn or not

--- reference-app/backend/app.py
+++ reference-app/backend/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, jsonify
-import logging
-
+import logging, os
+from prometheus_flask_exporter import PrometheusMetrics
 from prometheus_flask_exporter.multiprocess import GunicornInternalPrometheusMetrics
 
 from jaeger_client import Config
@@ -15,8 +15,11 @@ app.config['MONGO_URI'] = 'mongodb://example-mongodb-svc.default.svc.cluster.loc
 mongo = PyMongo(app)
 
 logging.basicConfig(level=logging.INFO)
-metrics = GunicornInternalPrometheusMetrics(app)
-metrics.info("app_info", "Backend Service", version="1.0.0")
+is_gunicorn = "gunicorn" in os.environ.get("SERVER_SOFTWARE", "")
+if is_gunicorn:
+    metrics = GunicornInternalPrometheusMetrics(app)
+else:
+    metrics = PrometheusMetrics(app)
 
 config = Config(
     config={
