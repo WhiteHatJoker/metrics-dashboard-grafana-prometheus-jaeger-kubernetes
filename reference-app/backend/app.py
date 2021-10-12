c5a15b9f19a31a7b00e2b8ca62caa5c79c02fe47
WhiteHatJoker <rkhudoyberdiev@gmail.com>
2021-10-13 02:56:42 +0500

    Added spans for tracing the backend

--- reference-app/backend/app.py
+++ reference-app/backend/app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template, request, jsonify
-import logging, os, random
+import logging, os, random, opentracing
 
 from prometheus_flask_exporter.multiprocess import GunicornInternalPrometheusMetrics
 
 from jaeger_client import Config
-from flask_opentracing import FlaskTracing
+from flask_opentracing import FlaskTracer
 
 import pymongo
 from flask_pymongo import PyMongo
@@ -31,32 +31,41 @@ config = Config(
 )
 
 jaeger_tracer = config.initialize_tracer()
-tracing = FlaskTracing(jaeger_tracer, True, app)
+tracing = FlaskTracer(jaeger_tracer, True, app)
+flask_head_tracing = tracing.get_span()
 
 @app.route('/')
 def homepage():
-    return "Hello World"
+    with opentracing.tracer.start_span("base-endpoint", child_of=flask_head_tracing) as span:
+        return "Hello World"
 
 
 @app.route('/api')
 def my_api():
-    answer = "something"
-    return jsonify(repsonse=answer)
+    with opentracing.tracer.start_span("api-endpoint", child_of=flask_head_tracing) as span:
+        answer = "something"
+        span.set_tag("answer", answer)
+        return jsonify(repsonse=answer)
 
 @app.route('/star', methods=['POST'])
 def add_star():
-  star = mongo.db.stars
-  name = request.json['name']
-  distance = request.json['distance']
-  star_id = star.insert({'name': name, 'distance': distance})
-  new_star = star.find_one({'_id': star_id })
-  output = {'name' : new_star['name'], 'distance' : new_star['distance']}
-  return jsonify({'result' : output})
+    with opentracing.tracer.start_span("star-endpoint", child_of=flask_head_tracing) as span:
+        try:
+            star = mongo.db.stars
+            name = request.json['name']
+            distance = request.json['distance']
+            star_id = star.insert({'name': name, 'distance': distance})
+            new_star = star.find_one({'_id': star_id })
+            output = {'name' : new_star['name'], 'distance' : new_star['distance']}
+            span.set_tag("output", output)
+            return jsonify({'result' : output})
+        except:
+            span.set_tag("output", "issue with database connection on star endpoint")
 
 @app.route('/errors')
 def error_message():
-  errors_choice = [500,503]
-  return jsonify({"error": "Fake Error",}), random.choice(errors_choice)
+    errors_choice = [500,503]
+    return jsonify({"error": "Fake Error",}), random.choice(errors_choice)
 
 if __name__ == "__main__":
     app.run()
